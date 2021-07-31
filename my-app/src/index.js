f2e53efce444c3dcf871ae0b47ad33272601560b
vinicius guterres <viniguter@gmail.com>
2021-07-31 18:57:36 -0300

    finish the project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,23 +2,89 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends Component {
-    render() {
-        return (
-            <button className='square'>
-                {/* TODO */}
-            </button>
-        );
-    };
+function Square(props) {
+    return (
+        <button 
+            className='square'
+            onClick={ props.onClick }
+        >
+            { props.value }
+        </button>
+    );
 };
 
 class Board extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    };
+
+    handleClick(i) {
+        // when click on the square, it will recive a value
+        const square = this.state.squares.slice();
+
+        // return case we got a winner
+        if (this.defineWinner(square) || square[i]) {
+            return;
+        };
+        
+        square[i] = this.state.xIsNext ? 'X' : 'O';
+
+        // will set the square value and toggle the player
+        this.setState({ 
+            squares: square,
+            xIsNext: !this.state.xIsNext
+        });
+    };
+
+    defineWinner(squares) {
+        // lines that make a winner 
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for(let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            
+            // case X or O mark all the lines, we got the winner
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            };
+        };
+        return null;
+    };
+
     renderSquare(i) {
-        return <Square />
+        // will render each square, passing the current index in the parameter
+        return(
+            <Square
+                value={ this.state.squares[i] }
+                onClick={ () => this.handleClick(i) }
+            />
+        );
     };
 
     render() {
-        const status = 'Next player: X';
+        const winner = this.defineWinner(this.state.squares);
+        let status;
+        
+        if (winner) {
+            status = `Winner ${winner}`;
+        } else {
+            status = `Next Player: ${ this.state.xIsNext ? 'X' : 'O' }`;
+        };
+
 
         return (
             <div>
